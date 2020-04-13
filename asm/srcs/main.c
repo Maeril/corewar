beac2c5d40cde70b8a1c7fcf958aa6b9bcf74e1f
Maeril <Maeril@Avesta.local>
2020-04-13 18:44:29 +0200

    13042020_1844_mae

--- asm/srcs/main.c
+++ asm/srcs/main.c
@@ -6,15 +6,48 @@
 /*   By: myener <myener@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/11 18:15:35 by myener            #+#    #+#             */
-/*   Updated: 2020/04/11 20:07:56 by myener           ###   ########.fr       */
+/*   Updated: 2020/04/13 18:43:20 by myener           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/asm.h"
 
-char	**get_file_content(char *file_name); // to be created (will use GNL)
+// void	asm_translator(int fd, char **input); // to be created (principal function)
+// {
+// 	int		i;
 
-void	asm_translator(int fd); // to be created (principal function)
+// 	i = 0;
+// 	while (input[i])
+// 	{
+// 		i++;
+// 	}
+// }
+
+char	**get_file_content(char *file_name)
+{
+	int		i;
+	int		fd;
+	char	*tmp;
+	char	*line;
+	char	*stock;
+	char	**content;
+
+	i = 0;
+	stock = ft_strnew(1);
+	fd = open(file_name, O_RDONLY);
+	while (get_next_line(fd, &line))
+	{
+		tmp = line;
+		stock = ft_free_join(stock, line);
+		stock = ft_free_join(stock, "\n");
+		free(tmp);
+		i++;
+	}
+	close(fd);
+	content = ft_strsplit(stock, '\0');
+	ft_strdel(&stock);
+	return (content);
+}
 
 int		main(int ac, char **av)
 {
@@ -29,8 +62,10 @@ int		main(int ac, char **av)
 	in_file_content = get_file_content(in_file_name);
 	out_file_name = ft_strsub(in_file_name, 0, ft_strlen(in_file_name) - 1); //copies input file name except "s"; "file.s" becomes "file."
 	out_file_name = ft_free_join(out_file_name, "cor"); // adds "cor" extension to file name; "file." becomes "file.cor"
-	fd = open(out_file_name, O_WRONLY); // opens out_file: it doesn't exists, so it creates it. open to WRITE in it.
-	asm_translator(fd); // writes in out_file (using the fd) the content of in_file but in machinelang.
+	ft_printf("out_file_name = %s\n", out_file_name);
+	fd = open(out_file_name, O_WRONLY | O_CREAT); // opens out_file: it doesn't exists, so it creates it. open to WRITE in it.
+	// asm_translator(fd, in_file_content); // writes the content of in_file in out_file, translated in machinelang.
+	// close(fd);
 	free(in_file_name);
 	free(out_file_name);
 	tab_free(in_file_content);
---
+++ asm/srcs/test.s
@@ -0,0 +1,7 @@
+One was strong.
+One unafraid of wanting, one like the sky.
+One who even dreaming of a paradise that would one day wither,
+Would, left casting shadows of happiness,
+Without knowing what to believe in,
+Harbor just the strength to love everything.
+Harbor just the weakness to accept everything.
