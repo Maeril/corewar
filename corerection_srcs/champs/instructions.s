b3cf0510592c1b95210e5c3c05bc5f74f5a4a9c3
Theo FERRIEUX <tferrieu@e2r4p16.42.fr>
2020-07-29 21:50:28 +0200

    fixed instructions.s

--- corerection_srcs/champs/instructions.s
+++ corerection_srcs/champs/instructions.s
@@ -4,7 +4,7 @@
 live	%1
 ld		%1, r1
 ld		1, r1
-st		r1, %1
+st		r1, r1
 st		r1, 1
 add		r1, r1, r1
 sub		r1, r1, r1
@@ -27,27 +27,23 @@ or		1, r1, r1
 or		1, %1, r1
 or		1, 1, r1
 xor		r1, r1, r1
-xor		r1, r1, %1
-xor		r1, r1, 1
 xor		r1, %1, r1
-xor		r1, %1, %1
-xor		r1, %1, 1
 xor		r1, 1, r1
-xor		r1, 1, %1
-xor		r1, 1, 1
+xor		%1, r1, r1
+xor		%1, %1, r1
+xor		%1, 1, r1
+xor		1, r1, r1
+xor		1, %1, r1
+xor		1, 1, r1
 zjmp	%1
-ldi		r1,	%1, r1
-ldi		r1, 1, r1
+ldi		r1, r1, r1
+ldi		r1, %1, r1
 ldi		%1, %1, r1
-ldi		%1, 1, r1
-ldi		1, %1, r1
-ldi		1, 1, r1
-sti		r1, r1,	%1
-sti		r1, r1, 1
+ldi		%1, r1, r1
+sti		r1, r1, r1
+sti		r1, r1, %1
 sti		r1, %1, %1
-sti		r1, %1, 1
-sti		r1, 1, %1
-sti		r1, 1, 1
+sti		r1, %1, r1
 fork	%1
 lld		%1, r1
 lld		1, r1
